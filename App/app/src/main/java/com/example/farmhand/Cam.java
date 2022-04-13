ae1ac6b9e5903abd602af1badaa2a03bdf3c42a7
Kowshique Roy <34647424+kowshiqueroy@users.noreply.github.com>
2022-04-14 00:25:26 +0600

    up

--- App/app/src/main/java/com/example/farmhand/Cam.java
+++ App/app/src/main/java/com/example/farmhand/Cam.java
@@ -2,17 +2,36 @@ package com.example.farmhand;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+//import com.google.android.gms.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Cam extends AppCompatActivity {
 
     private WebView mywebView;
+    Timer timer;
+    String url;
+    Button saveBtn;
+    TextView t;
+    ImageView imageView;
+   // ImageLabeler imageLabeler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +39,54 @@ public class Cam extends AppCompatActivity {
         setContentView(R.layout.activity_cam);
 
 
+        saveBtn= findViewById(R.id.button2);
+        t= findViewById(R.id.textView2);
+        imageView= findViewById(R.id.imageView);
+
+     //  imageLabeler= ImageLabeling.getClient()
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Ion.with(getApplicationContext())
+                        .load( )
+                        .withBitmap()
+                        //.placeholder(R.drawable.placeholder_image)
+                        //.error(R.drawable.error_image)
+                       // .animateLoad(spinAnimation)
+                       // .animateIn(fadeInAnimation)
+                        .intoImageView(imageView);
+
+
+            }
+        });
+
+
+
+        imageView.buildDrawingCache();
+        Bitmap bmap = imageView.getDrawingCache();
+        processImage();
+
         mywebView=(WebView) findViewById(R.id.webview);
         mywebView.setWebViewClient(new WebViewClient());
-        mywebView.loadUrl("http://192.168.1.13/");
+
+         url= "http://192.168.1.10/capture";
+      // mywebView.loadUrl(url);
+       // http://192.168.1.10:81/stream
+        mywebView.loadUrl( "http://192.168.1.10:81/stream");
+      //  mywebView.reload();
         WebSettings webSettings=mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
 
+
+
+
+
+
+    }
+
+    private void processImage() {
 
 
 
@@ -40,6 +100,10 @@ public class Cam extends AppCompatActivity {
         }
         @Override
         public boolean shouldOverrideUrlLoading(WebView view,String url){
+
+
+
+
             view.loadUrl(url);
             return true;
         }
